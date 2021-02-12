ea37f7377df9f0905520256a52050ddcaa4a2f04
Rufus Wong <rcywongaa@gmail.com>
2021-02-12 20:59:07 +0800

    !UNTESTED! Implement IK calculation

--- dev_ws/robot/src/Robot.cpp
+++ dev_ws/robot/src/Robot.cpp
@@ -1,6 +1,10 @@
+#include <array>
+
+#include <rclcpp/rclcpp.hpp>
+
 #include "Robot.hpp"
 
-#include <array>
+static const rclcpp::Logger LOGGER = rclcpp::get_logger("Robot");
 
 Robot::Robot(const robot_model_loader::RobotModelLoader& model_loader)
 {
@@ -11,14 +15,36 @@ Robot::Robot(const robot_model_loader::RobotModelLoader& model_loader)
 
 bool Robot::run(robot::msg::PointTrajectory traj)
 {
-  const moveit::core::JointModelGroup* joint_model_group = kinematic_model->getJointModelGroup("manipulator");
-  const Eigen::Isometry3d& end_effector_state = current_state->getGlobalLinkTransform("ee");
-  double timeout = 0.1;
-  bool is_found_ik = current_state->setFromIK(joint_model_group, end_effector_state, timeout);
-  if (is_found_ik)
+  for (geometry_msgs::msg::PointStamped point_stamped : traj.trajectory)
   {
-    // execute IK
-    ;
+    std::scoped_lock<std::mutex> lock(mtx);
+    const moveit::core::JointModelGroup* joint_model_group = kinematic_model->getJointModelGroup("manipulator");
+    Eigen::Isometry3d end_effector_state = Eigen::Isometry3d::Identity();
+    end_effector_state.translate(Eigen::Vector3d(
+          point_stamped.point.x,
+          point_stamped.point.y,
+          point_stamped.point.z));
+    double timeout = 0.1;
+    bool is_found_ik = current_state->setFromIK(joint_model_group, end_effector_state, timeout);
+    if (is_found_ik)
+    {
+      // execute IK
+      ;
+    }
+    else
+    {
+      RCLCPP_INFO(LOGGER, "Failed to find IK solution!");
+      return false;
+    }
   }
-  return is_found_ik;
+  return true;
+}
+
+void Robot::update(const sensor_msgs::msg::JointState::SharedPtr input)
+{
+  std::scoped_lock<std::mutex> lock(mtx);
+  for (unsigned int i = 0; i < input->name.size(); i++)
+  {
+    current_state->setJointPositions(input->name[i], &(input->position[i]));
+  }
 }
--- dev_ws/robot/src/Robot.hpp
+++ dev_ws/robot/src/Robot.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <mutex>
 
 #include <moveit/moveit_cpp/moveit_cpp.h>
 #include <moveit/robot_model_loader/robot_model_loader.h>
@@ -12,13 +13,13 @@ class Robot
   public:
     Robot(const robot_model_loader::RobotModelLoader& model_loader);
 
-    // TODO: Convert this into action server
+    // FIXME: Convert this into action server
     bool run(robot::msg::PointTrajectory traj);
 
     void update(const sensor_msgs::msg::JointState::SharedPtr input);
 
   private:
+    std::mutex mtx;
     moveit::core::RobotModelPtr kinematic_model;
     moveit::core::RobotStatePtr current_state;
-
 };
