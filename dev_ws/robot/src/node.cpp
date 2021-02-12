63b90589b41d7a34b2a51967f1cfd7302ce362c8
Rufus Wong <rcywongaa@gmail.com>
2021-02-12 21:00:24 +0800

    Fix bad pointer errors

--- dev_ws/robot/src/node.cpp
+++ dev_ws/robot/src/node.cpp
@@ -12,12 +12,23 @@ class RobotNode : public rclcpp::Node
 {
   public:
     RobotNode() :
-      Node("RobotNode")
-  {
-    std::string input_file = declare_and_get_parameter("input_file").as_string();
-    robot_model_loader::RobotModelLoader model_loader(shared_from_this(), std::string("robot_description"));
-    robot = std::make_unique<Robot>(model_loader);
-  }
+      rclcpp::Node("RobotNode")
+    {
+      RCLCPP_INFO(this->get_logger(), "RobotNode created!");
+    }
+
+    void init()
+    {
+      RCLCPP_INFO(this->get_logger(), "RobotNode initialized!");
+      robot_model_loader::RobotModelLoader model_loader(shared_from_this(), std::string("robot_description"));
+      traj_sub = this->create_subscription<robot::msg::PointTrajectory>("desired_trajectory", 1,
+          [&](const robot::msg::PointTrajectory::SharedPtr msg)
+          {
+            RCLCPP_INFO(this->get_logger(), "Received trajectory!");
+            robot->run(*msg);
+          });
+      robot = std::make_unique<Robot>(model_loader);
+    }
 
   private:
     rclcpp::Parameter declare_and_get_parameter(std::string param_name)
@@ -26,11 +37,15 @@ class RobotNode : public rclcpp::Node
       return this->get_parameter(param_name);
     }
     std::unique_ptr<Robot> robot;
+
+    rclcpp::Subscription<robot::msg::PointTrajectory>::SharedPtr traj_sub;
 };
 
 int main(int argc, char** argv)
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<RobotNode>());
+  std::shared_ptr<RobotNode> node = std::make_shared<RobotNode>();
+  node->init();
+  rclcpp::spin(node);
   rclcpp::shutdown();
 }
